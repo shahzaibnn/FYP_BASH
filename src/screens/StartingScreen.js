6cb32693e9f89c607e432115d414a6abe2a90a09
shahzaibnn <104250544+shahzaibnn@users.noreply.github.com>
2022-10-30 03:09:24 +0500

    Screen Creation

---
+++ src/screens/StartingScreen.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function StartingScreen() {
+  return (
+    <View>
+      <Text>StartingScreen</Text>
+    </View>
+  )
+}
