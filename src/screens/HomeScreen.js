cbdd32c18a24549a5e32822ae576797086e95dd8
habibafaisal <habibafaisal8@gmail.com>
2022-10-31 19:09:16 +0500

    Update HomeScreen.js

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -4,7 +4,13 @@ import React from 'react';
 export default function HomeScreen() {
   return (
     <View>
+<<<<<<< Updated upstream
       <Text>HomeScreen Test Habiba Another Branch</Text>
+=======
+      <Text>HomeScreen</Text>
+      <Text>BASH APPLICATION</Text>
+      <Text>BASH APPLICATION Habiba Another</Text>
+>>>>>>> Stashed changes
     </View>
   );
 }
