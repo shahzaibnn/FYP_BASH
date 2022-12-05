7c649b578b170582973ad9b469b8641bc971ef78
habibafaisal <habibafaisal8@gmail.com>
2022-12-05 22:35:12 +0500

    Firebase Test

---
+++ src/Firebase/Test.js
@@ -0,0 +1,111 @@
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
+import {useState} from 'react';
+import {ref, set, update, onValue, remove} from 'firebase/database';
+import {db, dbFirestore} from './Config';
+
+import firestore from '@react-native-firebase/firestore';
+
+export default function App() {
+  const [username, setName] = useState('');
+  const [email, setEmail] = useState('');
+
+  function createData() {
+    // const newKey = push(child(ref(database), 'users')).key;
+
+    set(ref(db, 'roles/' + username), {
+      username: username,
+      email: email,
+    })
+      .then(() => {
+        // Data saved successfully!
+        alert('data updated!');
+      })
+      .catch(error => {
+        // The write failed...
+        alert(error);
+      });
+  }
+
+  function updateData() {
+    // const newKey = push(child(ref(database), 'users')).key;
+
+    update(ref(db, 'users/' + username), {
+      username: username,
+      email: email,
+    })
+      .then(() => {
+        // Data saved successfully!
+        alert('data updated!');
+      })
+      .catch(error => {
+        // The write failed...
+        alert(error);
+      });
+  }
+
+  function readData() {
+    // const starCountRef = ref(db, 'users/' + username);
+    // onValue(starCountRef, snapshot => {
+    //   const data = snapshot.val();
+    //   setEmail(data.email);
+    // });
+  }
+
+  function deleteData() {
+    remove(ref(db, 'users/' + username));
+    alert('removed');
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text>Firebase crud!</Text>
+
+      <TextInput
+        value={username}
+        onChangeText={setName}
+        placeholder="Username"
+        style={styles.textBoxes}></TextInput>
+      <TextInput
+        value={email}
+        onChangeText={setEmail}
+        placeholder="Email"
+        style={styles.textBoxes}></TextInput>
+      <Button onPress={createData()} title="Get Data"></Button>
+      <TouchableOpacity>
+        {/* <Text onClick={createData()}>Submit Data </Text> */}
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text
+          onClick={() => {
+            console.log('submitted');
+          }}>
+          hi
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  textBoxes: {
+    width: '90%',
+    fontSize: 18,
+    padding: 12,
+    borderColor: 'gray',
+    borderWidth: 0.2,
+    borderRadius: 10,
+  },
+});
