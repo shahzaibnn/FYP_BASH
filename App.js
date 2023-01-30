bf9513f74e5ea225a0eb78eb0d7c7b1a420b4939
habibafaisal <habibafaisal8@gmail.com>
2023-01-30 20:52:16 +0500

    Update App.js

--- App.js
+++ App.js
@@ -55,6 +55,7 @@ export default function App() {
     </NavigationContainer>
     // <SettingsScreen />
 
+    //  <BottomTabs />
     // <JobPostingScreen />
     // <ViewJob />
     // <JobDashboardScreen />
