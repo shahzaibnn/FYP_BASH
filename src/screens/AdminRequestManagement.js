f28ee2e9da1d470dee27ce13728bb5516a086772
habibafaisal <habibafaisal8@gmail.com>
2023-02-26 20:21:31 +0500

    Account Approval Added (Request Management)

--- src/screens/AdminRequestManagement.js
+++ src/screens/AdminRequestManagement.js
@@ -20,6 +20,7 @@ import ActionSheet from 'react-native-actions-sheet';
 import {SliderBox} from 'react-native-image-slider-box';
 import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 import AntDesign from 'react-native-vector-icons/AntDesign';
+import Entypo from 'react-native-vector-icons/Entypo';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import {db, dbFirestore} from '../Firebase/Config';
 
@@ -30,10 +31,13 @@ const AdminRequestManagement = ({navigation}) => {
   const [postsSelected, setpostsSelected] = useState(false);
   const [jobsSelected, setjobsSelected] = useState(false);
   const [users, setUsers] = useState([]);
+  const [newUsersSelected, setNewUsersSelected] = useState(false);
+  const [usersUpdatesSelected, setUsersUpdatesSelected] = useState(false);
+  const [usersRemoveSelected, setUsersRemoveSelected] = useState(false);
 
   useEffect(() => {
     dbFirestore()
-      .collection('users')
+      .collection('Users')
       .where('accountApproved', '==', 'pending')
       .onSnapshot(querySnapshot => {
         const usersList = [];
@@ -43,6 +47,7 @@ const AdminRequestManagement = ({navigation}) => {
             key: documentSnapshot.id,
           });
         });
+        console.log(usersList);
         setUsers(usersList);
       });
   }, []);
@@ -50,26 +55,179 @@ const AdminRequestManagement = ({navigation}) => {
     return (
       <View>
         <View style={styles.expView}>
-          <View style={styles.userTabs}>
-            <TouchableOpacity>
+          <View
+            // onPress={() => setNewUsersSelected(true)}
+            style={{
+              width: Dimensions.get('window').width * 0.25,
+              height: Dimensions.get('window').height * 0.03,
+              borderRadius: 16,
+              marginTop: '15%',
+              // borderColor: 'black',
+              borderColor: '#4CA6A8',
+              backgroundColour: newUsersSelected ? '#4CA6A8' : '#ffffff',
+              borderWidth: 1,
+              marginLeft: '15%',
+            }}>
+            <TouchableOpacity onPress={() => setNewUsersSelected(true)}>
               {/* <AntDesign name="adduser" size={15} color="#4CA6A8" /> */}
               <Text style={styles.userTabStyles}>New Users</Text>
             </TouchableOpacity>
           </View>
-          <View style={styles.userTabs}>
+          <View
+            style={{
+              width: Dimensions.get('window').width * 0.25,
+              height: Dimensions.get('window').height * 0.03,
+              borderRadius: 16,
+              marginTop: '15%',
+              // borderColor: 'black',
+              borderColor: '#4CA6A8',
+              backgroundColour: usersUpdatesSelected ? '#4CA6A8' : '#ffffff',
+              borderWidth: 1,
+              marginLeft: '15%',
+            }}>
             <TouchableOpacity>
               <Text style={styles.userTabStyles}>Updates</Text>
             </TouchableOpacity>
           </View>
-          <View style={styles.userTabs}>
+          <View
+            style={{
+              width: Dimensions.get('window').width * 0.25,
+              height: Dimensions.get('window').height * 0.03,
+              borderRadius: 16,
+              marginTop: '15%',
+              // borderColor: 'black',
+              borderColor: '#4CA6A8',
+              backgroundColour: usersRemoveSelected ? '#4CA6A8' : '#ffffff',
+              borderWidth: 1,
+              marginLeft: '15%',
+            }}>
             <TouchableOpacity>
               <Text style={styles.userTabStyles}>Remove</Text>
             </TouchableOpacity>
           </View>
         </View>
+        {/* Flatlist to show accounts pending for approval */}
+        {newUsersSelected ? newUsersList() : null}
       </View>
     );
   };
+
+  const newUsersList = () => {
+    console.log('New users');
+    return (
+      <View>
+        <FlatList
+          data={users}
+          renderItem={({item}) => (
+            <View
+              // onPress={() => {
+              //   navigation.navigate('ViewProfile');
+              // }}
+              style={{
+                backgroundColor: 'rgba(187, 198, 200, 0.5)',
+                borderRadius: 16,
+                marginVertical: Dimensions.get('window').height * 0.05,
+                marginHorizontal: Dimensions.get('window').width * 0.05,
+                paddingBottom: '4%',
+              }}>
+              <View
+                style={{
+                  flexDirection: 'row',
+                }}>
+                <Image
+                  style={{
+                    marginLeft: Dimensions.get('window').width * 0.02,
+                    marginTop: Dimensions.get('window').width * 0.05,
+                    height: Dimensions.get('window').height * 0.1,
+                    width: Dimensions.get('window').width * 0.2,
+                    borderRadius: 16,
+                  }}
+                  source={{
+                    uri: item.pic,
+                  }}
+                />
+
+                <View
+                  style={{
+                    marginLeft: Dimensions.get('window').width * 0.03,
+                  }}>
+                  {/* <Text style={styles.designationStyle}>People</Text> */}
+                  <Text style={styles.designationStyle}>{item.role}</Text>
+                  {/* <Text>{item.firstName + ' ' + item.lastName}</Text> */}
+                  <View style={styles.ExpBoxView}>
+                    {/* <Text>{item.designation}</Text> */}
+                    <Text style={styles.ExpLocation}>
+                      {item.firstName + ' ' + item.lastName}
+                    </Text>
+                    <Text style={styles.ExpLocation}>Batch: {item.batch}</Text>
+                    <Text style={styles.ExpLocation}>
+                      Email: {item.userEmail}
+                    </Text>
+                    <Text style={styles.ExpLocation}>
+                      DOB: {item.dateOfBirth}
+                    </Text>
+                    <Text style={styles.ExpLocation}>
+                      Contact No: {item.contactNo}
+                    </Text>
+                  </View>
+                </View>
+              </View>
+              <View
+                style={{
+                  flexDirection: 'row',
+                  alignSelf: 'flex-end',
+                  marginEnd: '5%',
+                  marginTop: '8%',
+                }}>
+                <TouchableOpacity
+                  style={{marginRight: '4%'}}
+                  onPress={() => ApproveUser(item.key)}>
+                  <FontAwesome name="check-circle" size={26} color="green" />
+                </TouchableOpacity>
+                <TouchableOpacity onPress={RemoveUser(item.key)}>
+                  <FontAwesome name="times-circle" size={26} color="red" />
+                </TouchableOpacity>
+              </View>
+            </View>
+          )}
+          keyExtractor={item => item.id}
+        />
+      </View>
+    );
+  };
+
+  const ApproveUser = userId => {
+    console.log('approving user id: ', userId);
+    dbFirestore()
+      .collection('Users')
+      .get()
+      .then(querySnapshot => {
+        console.log('Total Found users: ', querySnapshot.size);
+
+        querySnapshot.forEach(documentSnapshot => {
+          // console.log(documentSnapshot.id);
+
+          dbFirestore()
+            .doc('Users/' + userId)
+            .update({
+              accountApproved: 'Approved',
+            })
+            .then(() => {
+              console.log('Approved in firestore');
+            })
+            .catch(err => {
+              console.log('not working');
+            });
+        });
+      })
+      .catch(error => {
+        alert(error);
+
+        // setFlag(true);
+      });
+  };
+  const RemoveUser = userId => {};
+
   const postsScreenmethod = () => {};
   const jobsScreenmethod = () => {};
 
@@ -250,6 +408,7 @@ const styles = StyleSheet.create({
     marginTop: '15%',
     // borderColor: 'black',
     borderColor: '#4CA6A8',
+    // backgroundColour: newUsersSelected ? '#4CA6A8' : '#ffffff',
     borderWidth: 1,
     marginLeft: '15%',
   },
@@ -425,6 +584,74 @@ const styles = StyleSheet.create({
   },
   text: {fontSize: 20, fontWeight: 'bold', color: '#6A6A6A', paddingTop: 15},
 
+  text: {
+    color: '#6A6A6A',
+    fontWeight: 'bold',
+    fontSize: 15,
+    marginLeft: 5,
+    marginTop: 20,
+  },
+  ExpBoxView: {
+    // flexDirection: 'column',
+    justifyContent: 'space-between',
+    marginTop: 10,
+  },
+  ExpLocation: {
+    textAlign: 'left',
+    // fontStyle: 'italic',
+    // justifyContent: 'flex-end',
+    // alignSelf: 'flex-end',
+    // alignItems: 'flex-end',
+  },
+  //   expView: {
+  //     flexDirection: 'row',
+  //     justifyContent: 'space-between',
+  //     width: Dimensions.get('window').width * 0.5,
+  //     alignSelf: 'center',
+  //   },
+  expView1: {
+    flexDirection: 'row',
+
+    justifyContent: 'space-between',
+    width: Dimensions.get('window').width * 0.5,
+    alignSelf: 'center',
+  },
+  editExpView: {
+    flex: 1,
+  },
+  designationStyle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: Dimensions.get('window').height * 0.006,
+    // marginHorizontal: Dimensions.get('window').width * 0.06,
+    color: '#000000',
+    marginTop: Dimensions.get('window').height * 0.02,
+  },
+  lastNameStyle: {
+    textAlign: 'right',
+    marginRight: 70,
+
+    fontSize: 15,
+    color: '#6A6A6A',
+    fontWeight: 'bold',
+    justifyContent: 'flex-end',
+    alignSelf: 'flex-end',
+    alignItems: 'flex-end',
+  },
+  header: {
+    backgroundColor: '#E5E3E4',
+    height: 100,
+    width: 100,
+    borderRadius: 64,
+    borderWidth: 10,
+    resizeMode: 'cover',
+    alignSelf: 'center',
+  },
+  Header: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
   text: {
     color: '#6A6A6A',
     fontWeight: 'bold',
